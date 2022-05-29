6494aaf1ce9037988d253b01427398a40422aacd
Tianyao Zhang <72031056+tylerzhang77@users.noreply.github.com>
2022-05-29 14:52:35 -0400

    Time: 350 ms (32.23%), Space: 0B (100.00%) - LeetHub

---
+++ 626-exchange-seats/626-exchange-seats.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+SELECT
+    (
+        CASE 
+            WHEN MOD(id, 2) = 1 AND id != (SELECT COUNT(*) FROM seat) THEN id+1
+            WHEN MOD(id, 2) = 0 THEN id-1
+            ELSE id
+        END
+    ) as id,
+    
+    student
+
+FROM seat
+
+ORDER BY 1
