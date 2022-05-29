8f037427c4749f65b516d63fc51bbc86db9d0ce3
Tianyao Zhang <72031056+tylerzhang77@users.noreply.github.com>
2022-05-29 14:49:27 -0400

    Time: 1121 ms (16.54%), Space: 0B (100.00%) - LeetHub

---
+++ 534-game-play-analysis-iii/534-game-play-analysis-iii.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT 
+    player_id, event_date,
+    sum(games_played) over(partition by player_id order by event_date) as games_played_so_far
+
+FROM activity
