f3fe8f765e58308db13139761d59d27a3e3f3d1d
Tianyao Zhang <72031056+tylerzhang77@users.noreply.github.com>
2022-05-30 19:18:20 -0400

    Time: 625 ms (26.97%), Space: 0B (100.00%) - LeetHub

---
+++ 1107-new-users-daily-count/1107-new-users-daily-count.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+with t1 as(
+    SELECT *,
+    row_number() OVER(partition by user_id ORDER BY activity_date ASC) as rk
+    FROM traffic
+    WHERE activity = 'login'
+)
+SELECT
+    activity_date as login_date,
+    COUNT(DISTINCT user_id) as user_count
+FROM t1
+
+WHERE DATEDIFF('2019-06-30', activity_date)<=90 AND rk = 1
+
+GROUP BY activity_date
