fe143841d59208849910a4fb5133c15d3f3aa171
Tianyao Zhang <72031056+tylerzhang77@users.noreply.github.com>
2022-05-31 10:03:20 -0400

    Time: 2370 ms (5.06%), Space: 0B (100.00%) - LeetHub

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+SELECT q1.person_name
+
+FROM queue q1 
+    JOIN queue q2 
+    ON q1.turn >= q2.turn
+    
+GROUP BY q1.turn
+
+HAVING SUM(q2.weight) <= 1000
+
+ORDER BY SUM(q2.weight) DESC
+
+LIMIT 1
+
