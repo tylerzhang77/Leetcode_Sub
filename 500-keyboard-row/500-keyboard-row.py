845125fc51a51aae94f8acbac8a6435d3f2a4246
Tianyao Zhang <72031056+tylerzhang77@users.noreply.github.com>
2022-06-02 21:16:11 -0400

    Time: 42 ms (52.83%), Space: 13.8 MB (97.60%) - LeetHub

---
+++ 500-keyboard-row/500-keyboard-row.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        set1 = {'q','w','e','r','t','y','u','i','o','p'}
+        set2 = {'a','s','d','f','g','h','j','k','l'}
+        set3 = {'z','x','c','v','b','n','m'}
+        res = []
+        for word in words:
+            wordset = set(word.lower())
+            if (wordset&set1 == wordset) or (wordset&set2 == wordset) or (wordset&set3 == wordset):
+                res.append(word)
+        return res
+                
