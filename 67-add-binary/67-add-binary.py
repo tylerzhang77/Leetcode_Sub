9f1c247009068b057181b79b22834363befeacd7
Tianyao Zhang <72031056+tylerzhang77@users.noreply.github.com>
2022-06-06 10:47:33 -0400

    Time: 56 ms (30.45%), Space: 13.8 MB (72.49%) - LeetHub

---
+++ 67-add-binary/67-add-binary.py
@@ -0,0 +1,18 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        carry = 0
+        res = ''
+        a = list(a)
+        b = list(b)
+        
+        while a or b or carry:
+            if a:
+                carry += int(a.pop())
+            if b:
+                carry += int(b.pop())
+                
+            res += str(carry % 2)
+            carry = carry // 2
+            
+        return res[::-1]
+                
