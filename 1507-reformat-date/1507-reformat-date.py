f4783582bbba537332d91d0be84a3abe2f8bcd49
Tianyao Zhang <72031056+tylerzhang77@users.noreply.github.com>
2022-06-05 21:43:01 -0400

    Time: 46 ms (38.66%), Space: 14 MB (19.33%) - LeetHub

---
+++ 1507-reformat-date/1507-reformat-date.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reformatDate(self, date: str) -> str:
+        s = date.split()
+        monthDict = {'Jan': '01', 'Feb': '02', 
+                     'Mar': '03', 'Apr': '04', 
+                     'May': '05', 'Jun': '06', 
+                     'Jul': '07', 'Aug': '08', 
+                     'Sep': '09', 'Oct': '10', 
+                     'Nov': '11', 'Dec': '12'}
+        day = s[0][:-2]
+        month = monthDict[s[1]]
+        year = s[2]
+        
+        if len(day) == 1:
+            day = '0' + day
+        
+        return f'{year}-{month}-{day}'
