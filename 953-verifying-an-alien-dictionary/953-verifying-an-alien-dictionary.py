43826eaf7d5f2cad1f012f60427d4608ebe3568a
Tianyao Zhang <72031056+tylerzhang77@users.noreply.github.com>
2022-06-12 21:53:30 -0400

    Time: 43 ms (72.19%), Space: 14 MB (37.41%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.py
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.py
@@ -5,6 +5,19 @@ class Solution:
         for a, b in zip(words, words[1:]):
             if len(a) > len(b) and a[:len(b)] == b:
                 return False
+            for c1, c2 in zip(a,b):
+                if ind[c1] < ind[c2]:
+                    break
+                elif ind[c1] > ind[c2]:
+                    return False
+        return True
+
+class Solution:
+    def isAlienSorted(self, words, order):
+        ind = {c: i for i, c in enumerate(order)}
+        for a, b in zip(words, words[1:]):
+            if len(a) > len(b) and a[:len(b)] == b:
+                return False
             for s1, s2 in zip(a, b):
                 if ind[s1] < ind[s2]:
                     break
