e98cce49f49ab87110a786049b3473c56756384d
Tianyao Zhang <72031056+tylerzhang77@users.noreply.github.com>
2022-06-14 13:10:48 -0400

    Time: 81 ms (11.15%), Space: 16.1 MB (81.25%) - LeetHub

--- 270-closest-binary-search-tree-value/270-closest-binary-search-tree-value.py
+++ 270-closest-binary-search-tree-value/270-closest-binary-search-tree-value.py
@@ -6,18 +6,15 @@
 #         self.right = right
 class Solution:
     def closestValue(self, root: Optional[TreeNode], target: float) -> int:
-        mindif = inf
-        res = 0
-        def helper(node):
-            nonlocal mindif
-            nonlocal res
-            if node:
-                if abs(node.val - target) < mindif:
-                    mindif = abs(node.val - target)
-                    res = node.val
-                helper(node.left)
-                helper(node.right)
-        helper(root)
+        res = root.val
+        while root:
+            if abs(root.val - target) < abs(res - target):
+                res = root.val
+            if root.val < target:
+                root = root.right
+            else:
+                root = root.left
+                
         return res
                 
             
