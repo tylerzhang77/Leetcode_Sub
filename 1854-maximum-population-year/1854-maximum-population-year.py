fb25a98afe93f81246f5cae62a49bb805e45d542
Tianyao Zhang <72031056+tylerzhang77@users.noreply.github.com>
2022-06-14 10:31:17 -0400

    Time: 92 ms (10.83%), Space: 13.8 MB (72.66%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.py
+++ 1854-maximum-population-year/1854-maximum-population-year.py
@@ -1,15 +1,16 @@
 class Solution:
     def maximumPopulation(self, logs: List[List[int]]) -> int:
-        logs = sorted(logs, key = lambda x: x[0])
-        resdic = {}
-        for i in range(len(logs)):
-            a, b = logs[i][0], logs[i][1]
-            counter = 1
-            for j in range(i):
-                c, d = logs[j][0], logs[j][1]
-                if d > a:
-                    counter += 1
-            resdic[a] = counter
-        return max(resdic, key = resdic.get)
+        log = []
+        for live, death in logs:
+            log.append((live, 1))
+            log.append((death, -1))
+        log.sort()
+        currpop = maxpop = maxyear = 0
+        for year, ind in log:
+            currpop += ind
+            if currpop > maxpop:
+                maxpop = currpop
+                maxyear = year
+        return maxyear
             
                 
