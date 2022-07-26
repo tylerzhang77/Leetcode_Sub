a81844827ca9c0a0af2874cc2dfe5fa7b29b8f26
Tianyao Zhang <72031056+tylerzhang77@users.noreply.github.com>
2022-07-26 14:21:36 -0400

    Time: 90 ms (23.29%), Space: 15.3 MB (92.20%) - LeetHub

---
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.py
@@ -0,0 +1,25 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        queue = [root]
+        while queue and root:
+            n = len(queue)
+            for i in range(n):
+                rt = queue.pop(0)
+                if rt.left:
+                    queue.append(rt.left)
+                if rt.right:
+                    queue.append(rt.right)
+                if i == n-1:
+                    break
+                rt.next = queue[0]
+        return root
