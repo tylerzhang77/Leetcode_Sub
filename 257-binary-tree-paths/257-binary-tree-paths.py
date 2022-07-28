2d47682651e47f28d8ec481da8eef454a10733cf
Tianyao Zhang <72031056+tylerzhang77@users.noreply.github.com>
2022-07-28 10:22:41 -0400

    Time: 64 ms (17.71%), Space: 14 MB (28.63%) - LeetHub

---
+++ 257-binary-tree-paths/257-binary-tree-paths.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        # 题目中节点数至少为1
+        stack, path_st, result = deque([root]), deque(), []
+        path_st.append(str(root.val))
+
+        while stack:
+            cur = stack.pop()
+            path = path_st.pop()
+            # 如果当前节点为叶子节点，添加路径到结果中
+            if not (cur.left or cur.right):
+                result.append(path)
+            if cur.right:
+                stack.append(cur.right)
+                path_st.append(path + '->' + str(cur.right.val))
+            if cur.left:
+                stack.append(cur.left)
+                path_st.append(path + '->' + str(cur.left.val))
+
+        return result        
