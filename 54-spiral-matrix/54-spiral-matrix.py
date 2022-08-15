7404c434e05589d558f683ccfd654b26fb8e070a
Tianyao Zhang <72031056+tylerzhang77@users.noreply.github.com>
2022-08-15 11:44:56 -0400

    Time: 42 ms (67.23%), Space: 13.8 MB (99.04%) - LeetHub

---
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -0,0 +1,7 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        res = []
+        while matrix:
+            res.extend(matrix.pop(0))
+            matrix = [*zip(*matrix)][::-1]
+        return res
