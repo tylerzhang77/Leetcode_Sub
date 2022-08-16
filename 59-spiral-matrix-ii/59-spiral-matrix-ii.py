3f6a615b076ea82a2d3119ce0de80d4d855bd2ef
Tianyao Zhang <72031056+tylerzhang77@users.noreply.github.com>
2022-08-16 10:36:24 -0400

    Time: 38 ms (83.58%), Space: 13.9 MB (83.07%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.py
@@ -0,0 +1,7 @@
+class Solution:
+    def generateMatrix(self, n):
+        A, lo = [], n*n+1
+        while lo > 1:
+            lo, hi = lo - len(A), lo
+            A = [range(lo, hi)] + [*zip(*A[::-1])]
+        return A
