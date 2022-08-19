97dd2a91b54531b780b73e9b02f04a71838e482b
Tianyao Zhang <72031056+tylerzhang77@users.noreply.github.com>
2022-08-19 09:41:59 -0400

    Time: 427 ms (72.03%), Space: 16 MB (52.30%) - LeetHub

---
+++ 77-combinations/77-combinations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        path = []
+        def backtrack(n, k, StartIndex):
+            if len(path) == k:
+                res.append(path[:])
+                return
+            for i in range(StartIndex, n + 1):
+                path.append(i)
+                backtrack(n, k, i+1)
+                path.pop()
+        backtrack(n, k, 1)
+        return res
